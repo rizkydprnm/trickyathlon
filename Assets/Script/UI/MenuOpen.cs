a9a59ff64c4f234b3ddb7452fc4589f3cbd0255f
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2025-05-14 08:03:20 +0700

    implementasi animasi main menu

--- Assets/Script/UI/MenuOpen.cs
+++ Assets/Script/UI/MenuOpen.cs
@@ -4,15 +4,17 @@ using PrimeTween;
 
 public class MenuOpen : MonoBehaviour
 {
-    [SerializeField] private GameObject thisMenu;
     [SerializeField] private GameObject nextMenu;
+
+    private GameObject thisMenu;
 
     public void Execute()
     {
+        thisMenu = gameObject.transform.parent.gameObject;
         Sequence.Create()
             .ChainCallback(()=>GetComponent<Button>().interactable = false)
             .Chain(Tween.UIAnchoredPositionX(
-                thisMenu.GetComponent<RectTransform>(), startValue: 0, endValue: 200, duration: 0.25f, Ease.OutCirc)
+                thisMenu.GetComponent<RectTransform>(), startValue: 0, endValue: 200, duration: 0.125f, Ease.OutCirc)
             )
             .ChainCallback(() =>
             {
@@ -20,6 +22,6 @@ public class MenuOpen : MonoBehaviour
                 thisMenu.SetActive(false);
                 nextMenu.SetActive(true);
             })
-            .Chain(Tween.UIAnchoredPositionX(nextMenu.GetComponent<RectTransform>(), startValue: 200, endValue: 0, duration: 0.25f, Ease.InCirc));
+            .Chain(Tween.UIAnchoredPositionX(nextMenu.GetComponent<RectTransform>(), startValue: 200, endValue: 0, duration: 0.125f, Ease.InCirc));
     }
 }
